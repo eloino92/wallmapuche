225a8ed17b526397d8de2ecaaee81481f4cb7390
eloino92 <gerardo.canio@outlook.com>
2023-08-10 20:29:36 -0400

    update progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-window.addEventListener("DOMContentLoaded", (loadEvent) => {
-    $("header").load("assets/includes/header.html");
-    $("footer").load("assets/includes/footer.html");
-});
+// Seleccionar barra de progreso 
+const progress = document.querySelector('progress');
 
+// Mostrar barra de progreso
+progress.style.display = 'block'; 
+
+window.addEventListener("DOMContentLoaded", (loadEvent) => {
+
+  // Cargar header y footer
+  $("header").load("assets/includes/header.html");
+  $("footer").load("assets/includes/footer.html");
+
+  // Ocultar barra de progreso cuando termine la carga
+  progress.style.display = 'none';
+
+});
