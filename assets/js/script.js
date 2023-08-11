c5fc25972cd9c15aa7c9830ad52fee758edcc936
eloino92 <gerardo.canio@outlook.com>
2023-08-10 22:11:49 -0400

    delete progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,4 @@
-// Seleccionar barra de progreso 
-const progress = document.querySelector('progress');
-
-// Mostrar barra de progreso
-progress.style.display = 'block'; 
-
 window.addEventListener("DOMContentLoaded", (loadEvent) => {
-
-  // Cargar header y footer
-  $("header").load("assets/includes/header.html");
-  $("footer").load("assets/includes/footer.html");
-
-  // Ocultar barra de progreso cuando termine la carga
-  progress.style.display = 'none';
-
+    $("header").load("assets/includes/header.html");
+    $("footer").load("assets/includes/footer.html");
 });
